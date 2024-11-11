eaa21c86a59b6eed244fd491ef044caf839870fd
szczescie <105247530+szczescie@users.noreply.github.com>
2024-11-11 11:25:03 +0100

    added logging to main

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 //! Manual tiling without boilerplate key presses.
 
 const std = @import("std");
+const log = std.log;
 const mem = std.mem;
 const os = std.os;
 const posix = std.posix;
@@ -8,11 +9,17 @@ const posix = std.posix;
 const Socket = @import("Socket.zig");
 const Columns = @import("Columns.zig");
 
+const version = "0.1";
+
 /// Run the program.
 pub fn main() (Socket.ErrorSwaysock || Socket.ErrorWriteRead)!void {
     var buf: [512 * 1024]u8 = undefined;
     const columns = try Columns.init(&buf);
     defer columns.deinit();
+
+    log.info("swaycolumns version {s} started; good morning 🌞", .{version});
+    log.debug("# of cli arguments: {d}", .{os.argv.len - 1});
+    log.debug("cli arguments: {s}", .{os.argv[1..]});
 
     if (os.argv.len == 3) {
         const subcommands = .{
@@ -43,9 +50,10 @@ pub fn main() (Socket.ErrorSwaysock || Socket.ErrorWriteRead)!void {
         if (mem.eql(u8, mem.span(os.argv[1]), "start")) {
             try columns.layoutStart();
         }
-    } else {
-        posix.exit(1);
     }
+
+    log.info("no actionable arguments; exiting", .{});
+    posix.exit(1);
 }
 
 // TODO: logs
